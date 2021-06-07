5bfb4df5f7e4f1577cdbc551395cb6fa769a2195
D.Shiltsov <shiltsov@gmail.com>
2021-06-07 22:36:22 +0300

    start2

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),    
+    path('portfolio/', views.portfolio, name="portfolio"),    
+    path('contacts/', views.contacts, name="contacts"),    
     path('admin/', admin.site.urls),
 ]
